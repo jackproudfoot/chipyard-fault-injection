1de64ca05ad9cbd373e9ba8dd5a33f16ec34ae08
Jack Proudfoot <jackson.proudfoot@gmail.com>
2022-11-05 14:25:03 -0400

    Add basic funcs

--- injector/main.py
+++ injector/main.py
@@ -1,3 +1,9 @@
+from mapping import Module
 
 if __name__ == '__main__':
-    print('hello')
+    mod = Module('test', 'test module')
+
+    mod2 = Module('test2', 'test module 2', parents = [mod])
+
+    print(mod)
+    print(mod2)
--- injector/mapping.py
+++ injector/mapping.py
@@ -1,6 +1,30 @@
 class Module:
-    # moduleName = ""     track internally?
-    io_dict = dict()     #wirename -> input/output     
-    parents = []        #list of other Modules that call this module, easy to move up graph
-    module_text = ""     #entire text of module verilog
 
+    def __init__(self, name, module_text, parents = [], children = []):
+        self.name = name
+        self.module_text = module_text
+        self.io = dict()
+        self._parents = parents
+        self._children = children
+
+        if module_text == '':
+            print('Cannot initialize module ({}) without module text.'.format(name))
+            exit(1)
+
+        self._parse_io()
+
+    def add_parents(self, parents):
+        self._parents += parents
+
+    def add_children(self, children):
+        self._children += children
+
+    def _parse_io(self):
+        pass
+
+    def __str__(self):
+        return '== {} ==\n\tParents: {}\n\tChildren: {}'.format(
+            self.name, 
+            [parent.name for parent in self._parents], 
+            [child.name for child in self._children]
+        )
